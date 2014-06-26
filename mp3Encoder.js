ff7c38418bf222335c8138edc5f91a58c0c1b7ad
rakesh <rakesh.kumar@gmail.com>
2014-06-26 17:07:21 +0530

    added encodeDecodeMp3.js

--- mp3Encoder.js
+++ mp3Encoder.js
@@ -4,14 +4,14 @@
 
 var fs = require('fs');
 
-var encodeMp3 = require('./util/encodeMp3.js');
+var encodeMp3 = require('./util/encodeDecodeMp3.js');
 var decodeMp3 = require('./util/decodeMp3.js');
 
 //console.log(data);
 
-encodeMp3.encodeMp3('/home/rakesh/Downloads/Enemmy-Bheege Naina.mp3','b64');
-encodeMp3.encodeMp3('/home/rakesh/Downloads/Enemmy-Bheege Naina.mp3','txt');
-encodeMp3.encodeMp3('/home/rakesh/Downloads/Enemmy-Bheege Naina.mp3','hex');
+encodeDecodeMp3.encodeMp3('/home/rakesh/Downloads/Enemmy-Bheege Naina.mp3','b64');
+encodeDecodeMp3.encodeMp3('/home/rakesh/Downloads/Enemmy-Bheege Naina.mp3','txt');
+encodeDecodeMp3.encodeMp3('/home/rakesh/Downloads/Enemmy-Bheege Naina.mp3','hex');
 
 console.log('start Decode');
 decodeMp3.decodeMp3('Encoded Files/Enemmy-Bheege Naina.hex');
